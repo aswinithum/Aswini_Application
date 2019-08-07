31b87a53c3c57e3c334e1160f84a63c2ba48fd91
Aswini Thummala <aswinithummala1996@outlook.com>
2019-08-07 14:12:04 +0530

    aswini changes

---
+++ Aswini_FirstApp/Aswini_FirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Aswini_FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "aswini";
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
